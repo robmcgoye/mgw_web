be99481fd37c279878713a9d80a6b5dc5b6318e2
Rob McGoye <rob@mcgoye.com>
2022-05-18 11:05:00 -0400

    Fixed issue with images

--- app/javascript/world_map.js
+++ app/javascript/world_map.js
@@ -1,27 +1,27 @@
 $(document).on('turbolinks:load', function() {
 
   $("#south_america_map_r1c1").mouseover(function() {
-    $("#map_r1_c1").attr ("src", "/assets/map_r1_c1_f3.gif");
+    $("#map_r1_c1").attr ("src", "/images/map_r1_c1_f3.gif");
   }).mouseout(function(){
-    $("#map_r1_c1").attr ("src", "/assets/map_r1_c1.gif");
+    $("#map_r1_c1").attr ("src", "/images/map_r1_c1.gif");
   });
 
   $("#north_america_map_r1c1").mouseover(function() {
-    $("#map_r1_c1").attr ("src", "/assets/map_r1_c1_f2.gif");
+    $("#map_r1_c1").attr ("src", "/images/map_r1_c1_f2.gif");
   }).mouseout(function(){
-    $("#map_r1_c1").attr ("src", "/assets/map_r1_c1.gif");
+    $("#map_r1_c1").attr ("src", "/images/map_r1_c1.gif");
   });
   
   $("#africa_map_r3c2").mouseover(function() {
-    $("#map_r3_c2").attr ("src", "/assets/map_r3_c2_f4.gif");
+    $("#map_r3_c2").attr ("src", "/images/map_r3_c2_f4.gif");
   }).mouseout(function(){
-    $("#map_r3_c2").attr ("src", "/assets/map_r3_c2.gif");
+    $("#map_r3_c2").attr ("src", "/images/map_r3_c2.gif");
   });
 
   $("#australia_map_r4c5").mouseover(function() {
-    $("#map_r4_c5").attr ("src", "/assets/map_r4_c5_f6.gif");
+    $("#map_r4_c5").attr ("src", "/images/map_r4_c5_f6.gif");
   }).mouseout(function(){
-    $("#map_r4_c5").attr ("src", "/assets/map_r4_c5.gif");
+    $("#map_r4_c5").attr ("src", "/images/map_r4_c5.gif");
   });
 
   // Asia start
@@ -53,19 +53,19 @@ $(document).on('turbolinks:load', function() {
   // Asia end
 
   function asia_over() {
-    $("#map_r2_c3").attr ("src", "/assets/map_r2_c3_f5.gif");
-    $("#map_r3_c2").attr ("src", "/assets/map_r3_c2_f5.gif");
-    $("#map_r3_c4").attr ("src", "/assets/map_r3_c4_f5.gif");
-    $("#map_r4_c4").attr ("src", "/assets/map_r4_c4_f5.gif");
-    $("#map_r4_c5").attr ("src", "/assets/map_r4_c5_f5.gif");
+    $("#map_r2_c3").attr ("src", "/images/map_r2_c3_f5.gif");
+    $("#map_r3_c2").attr ("src", "/images/map_r3_c2_f5.gif");
+    $("#map_r3_c4").attr ("src", "/images/map_r3_c4_f5.gif");
+    $("#map_r4_c4").attr ("src", "/images/map_r4_c4_f5.gif");
+    $("#map_r4_c5").attr ("src", "/images/map_r4_c5_f5.gif");
   }
 
   function asia_out() {
-    $("#map_r2_c3").attr ("src", "/assets/map_r2_c3.gif");
-    $("#map_r3_c2").attr ("src", "/assets/map_r3_c2.gif");
-    $("#map_r3_c4").attr ("src", "/assets/map_r3_c4.gif");
-    $("#map_r4_c4").attr ("src", "/assets/map_r4_c4.gif");
-    $("#map_r4_c5").attr ("src", "/assets/map_r4_c5.gif");
+    $("#map_r2_c3").attr ("src", "/images/map_r2_c3.gif");
+    $("#map_r3_c2").attr ("src", "/images/map_r3_c2.gif");
+    $("#map_r3_c4").attr ("src", "/images/map_r3_c4.gif");
+    $("#map_r4_c4").attr ("src", "/images/map_r4_c4.gif");
+    $("#map_r4_c5").attr ("src", "/images/map_r4_c5.gif");
   }
 
 });
